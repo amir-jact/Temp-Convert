ea26b55b6e54cc3f188d2ddc79eb36d9cbecfac2
amir_jact <61495812+amir-jact@users.noreply.github.com>
2022-03-10 20:22:10 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+﻿let change = document.querySelector(".changeButton")
+let convert = document.querySelector(".convertButton")
+let restB = document.querySelector(".resetButton")
+let cel = document.querySelector(".C")
+let far = document.querySelector(".F")
+let con = document.getElementById("converter")
+let res  = document.querySelector(".result")
+
+
+function changeFun(){
+    if(cel.innerHTML == "°C"){
+        document.title = "Js | °F to °C"
+        cel.innerHTML = "°F"
+        far.innerHTML = "°C"
+        // con.placeholder = "°F"
+        con.setAttribute("placeholder", "°F")
+    }else{
+        document.title = "Js | °C to °F"
+        cel.innerHTML = "°C"
+        far.innerHTML = "°F"
+        con.placeholder = "°C"
+    }
+
+}
+
+function convertFun(){
+    let c = con.value
+    if(!isFinite(c) || c ===""){
+        res.innerText = 'oh no give me a number'
+    }else{
+        if(cel.innerHTML == "°C"){
+            res.innerText = (parseFloat(c) * (9 / 5) + 32).toFixed(2) + " F°"
+        }else{
+            res.innerText = ((parseFloat(c)-32) * (5 / 9)).toFixed(2) + " C°"
+        }
+    }
+}
+
+function resFun(){
+    document.title = "Js | °C to °F"
+    cel.innerHTML = "°C"
+    far.innerHTML = "°F"
+    res.innerText = ""
+    con.placeholder = "°C"
+    con.value = ""
+}
+
+change.addEventListener("click", changeFun)
+convert.addEventListener("click", convertFun)
+restB.addEventListener("click", resFun)
